996e78d40c90140f971771b3a467c473da911bb7
Rinat Khusainov <khusainovrm@yandex.ru>
2021-02-08 20:46:58 +0300

    meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,10 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        name: 'facebook-domain-verification',
+        content: 'c8milld61juhe5hirkms3w9wib1wa0',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
